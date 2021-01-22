253c21fc0d898dd1ffbfc15bb9ea70f5b0a24909
Joey Faherty <joeyfaherty@live.ie>
2021-01-22 10:45:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os, sys
 
-def anogram_check(s1, s2):
+def anagram_check(s1, s2):
   # Strings are sorted, transformed to lower case and verified
   if(sorted(s1.lower())== sorted(s2.lower())):
-    print("Both strings form an anogram.")
+    print("Both strings form an anagram.")
   else:
-    print("Both strings do not form an anogram.")
+    print("Both strings do not form an anagram.")
   
 s1 =  sys.argv[1]
 s2 =  sys.argv[2]
 print("String input s1: ", s1 )
 print("String input s2: ", s2 )
-anogram_check(s1, s2)
+anagram_check(s1, s2)
 
 
